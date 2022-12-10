cf1eaa60a85e1ebd64bcc9e40a11166aad383c83
_DEN_ <kononchukd04@gmail.com>
2022-12-10 19:52:10 +0300

    configure BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import {
-	BrowserRouter,
-	RedirectFunction,
-	Route,
-	Routes,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import NavigationBar from "./infoblocks/Navigation/NavigationBar";
+import Home from "./pages/Home";
+import ArchitectPersonalInfo from "./pages/ArchitectPersonalInfo";
 
 function App() {
 	return (
-		<>
-			<BrowserRouter>
-				<Routes></Routes>
-			</BrowserRouter>
-		</>
+		<div>
+			<Routes>
+				<Route path="Architects-of-Belarus/" element={<NavigationBar />}>
+					<Route index element={<Home />} />
+					<Route path="architects" element={<h1> asldf</h1>} />
+					{/* <Route path="/architects/:id" element={ArchitectPersonalInfo} /> */}
+				</Route>
+			</Routes>
+		</div>
 	);
 }
 
